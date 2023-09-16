08d5b3cd10be4a9500fcb511fedec8ed7cbfcc42
Dat Ngo <dat.ngo2994@gmail.com>
2023-09-16 15:55:30 +0700

    update .env

--- api.js
+++ api.js
@@ -1,8 +1,5 @@
 const puppeteer = require('puppeteer');
 require('dotenv').config();
-const fetch = require('node-fetch');
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
 const { MongoClient } = require('mongodb');
 
@@ -15,6 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(loginToGetCookie);
+
+app.get('/', (req, res) => {
+  res.json({ cookie: globalCookie });
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
@@ -61,7 +62,7 @@ async function loginToGetCookie(req, res, next) {
 
 async function saveCookieToMongoDB(cookie) {
   try {
-    const uri = 'mongodb+srv://datngo2994:D%40t12345@cluster0.qjwl5f2.mongodb.net/';
+    const uri = process.env.MONGODB_URI;
     const client = new MongoClient(uri, { useUnifiedTopology: true });
     await client.connect();
 
@@ -83,87 +84,3 @@ async function saveCookieToMongoDB(cookie) {
     console.error(error);
   }
 }
-
-// async function fetchCustomerData(req, res, next) {
-//   console.log('Fetching customer data');
-//   console.log(globalCookie);
-//   if (globalCookie) {
-//     try {
-//       let page = 1;
-//       const limit = 100;
-//       let allCustomers = [];
-
-//       while (true) {
-//         const url = `https://patronusjewelry.mysapogo.com/admin/customers/doSearch.json?page=${page}&limit=${limit}&condition_type=must`;
-//         const response = await fetch(url, {
-//           headers: {
-//             cookie: globalCookie,
-//           },
-//           method: 'GET',
-//         });
-
-//         const data = await response.json();
-//         console.log(data.customers);
-
-//         allCustomers = allCustomers.concat(data.customers);
-
-//         if (data.customers.length < limit) {
-//           break;
-//         }
-
-//         page++;
-//       }
-
-//       const customerIds = allCustomers.map((customer) => customer.id);
-
-//       const fileName = 'customerData.json';
-//       fs.writeFileSync(fileName, JSON.stringify(customerIds, null, 2));
-
-//       res.json(allCustomers);
-//     } catch (error) {
-//       console.error(error);
-//       res.sendStatus(500);
-//     }
-//   } else {
-//     next();
-//   }
-// }
-
-// app.get('/getData', fetchCustomerData);
-
-// async function fetchDetailCustomerData(req, res) {
-//   try {
-//     const id = req.params.id;
-//     const url = `https://patronusjewelry.mysapogo.com/admin/loyalty/v2/customers/${id}.json`;
-
-//     const response = await fetch(url, {
-//       headers: {
-//         cookie: globalCookie,
-//       },
-//       method: 'GET',
-//     });
-
-//     const data = await response.json();
-//     console.log(data);
-
-//     const customerDataJSON = JSON.stringify(data, null, 2);
-//     const fileName = `${id}.json`;
-//     const folderPath = path.join(__dirname, 'customers');
-//     const filePath = path.join(folderPath, fileName);
-
-//     if (!fs.existsSync(folderPath)) {
-//       fs.mkdirSync(folderPath);
-//     }
-
-//     fs.writeFileSync(filePath, customerDataJSON);
-
-//     console.log(`Customer data saved to ${filePath}`);
-
-//     res.sendStatus(200);
-//   } catch (error) {
-//     console.error(error);
-//     res.sendStatus(500);
-//   }
-// }
-
-// app.get('/detail/:id', fetchDetailCustomerData);
